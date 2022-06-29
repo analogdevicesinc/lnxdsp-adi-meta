71fcbb00ce1c3fb528e7120dbc2b8726bf81f9df
Nathan Barrett-Morrison <nathan.morrison@timesys.com>
2022-06-29 09:29:29 -0400

    SC584: Add in remoteproc example

---
+++ meta-adi-adsp-sc5xx/recipes-icc/remoteproc-examples/remoteproc-examples-sc584/load_mcapi.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo stop > /sys/class/remoteproc/remoteproc0/state
+echo stop > /sys/class/remoteproc/remoteproc1/state
+
+mkdir -p /lib/firmware
+
+if [ ! -f /lib/firmware/mcapi_send_recv_sc584_Core1.ldr ]; then
+	ln -s /remoteproc/mcapi_send_recv_sc584_Core1.ldr /lib/firmware/mcapi_send_recv_sc584_Core1.ldr
+fi
+
+if [ ! -f /lib/firmware/mcapi_send_recv_sc584_Core2.ldr ]; then
+	ln -s /remoteproc/mcapi_send_recv_sc584_Core2.ldr /lib/firmware/mcapi_send_recv_sc584_Core2.ldr
+fi
+
+
+echo mcapi_send_recv_sc584_Core1.ldr > /sys/class/remoteproc/remoteproc0/firmware
+echo mcapi_send_recv_sc584_Core2.ldr > /sys/class/remoteproc/remoteproc1/firmware
+echo start > /sys/class/remoteproc/remoteproc0/state
+echo start > /sys/class/remoteproc/remoteproc1/state
+
+sleep 1
+
+arm_sharc_msg_demo -r 5
