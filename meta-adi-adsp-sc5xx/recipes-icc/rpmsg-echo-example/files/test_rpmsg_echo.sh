c9934617ef6c91a5872635a5d9f07fe52f515a94
Greg Malysa <gmalysa@alumni.stanford.edu>
2022-10-27 23:35:23 -0400

    All SoCs: remove mcapi and old remoteproc examples; add rpmsg echo example program and test script

---
+++ meta-adi-adsp-sc5xx/recipes-icc/rpmsg-echo-example/files/test_rpmsg_echo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "echo_core1.ldr" > /sys/class/remoteproc/remoteproc0/firmware
+echo "start" > /sys/class/remoteproc/remoteproc0/state
+
+sleep 2
+
+echo "echo_core2.ldr" > /sys/class/remoteproc/remoteproc1/firmware
+echo "start" > /sys/class/remoteproc/remoteproc1/state
+
+sleep 2
+
+RPMSG_EP=$(basename $(ls -d /sys/bus/rpmsg/devices/*.sharc-echo.-1.151))
+rpmsg-bind-chardev -d ${RPMSG_EP} -a 50
+echo hello | rpmsg-xmit -n 5 /dev/rpmsg0
+RPMSG_EP=$(basename $(ls -d /sys/bus/rpmsg/devices/*.sharc-echo-cap.-1.161))
+rpmsg-bind-chardev -d ${RPMSG_EP} -a 61
+echo hello | rpmsg-xmit -n 5 /dev/rpmsg1
