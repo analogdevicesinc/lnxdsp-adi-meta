14a44ca5ee8c73bfa849a3d5f5c0cddd3dc05713
Utsav Agarwal <utsav.agarwal@analog.com>
2025-06-11 12:20:00 +0100

    Kitchen sink enable using bash script
    Signed-off-by: Utsav Agarwal <utsav.agarwal@analog.com>

---
+++ tools/prepare-kitchen-sink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Preparing kitchen sink sources..."
+
+# Jupyter
+cd ..
+echo "Cloning Xilinx Jupyter meta layer..."
+git clone https://github.com/Xilinx/meta-jupyter meta-xilinx
+echo "Patching Xilinx meta layer..."
+cd meta-xilinx
+git apply ../meta-adi/tools/kitchen_sink_patches/000*.patch
+
+# meta-oe
+cd ..
+echo "Patching OE meta layer..."
+cd meta-openembedded/meta-oe/
+git apply ../../meta-adi/tools/kitchen_sink_patches/graphviz.patch
